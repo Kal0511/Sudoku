1c93f07109c00cfdfe6d930ecc7480e38b7d2bd3
Kaumil Patel <spdmode@gmail.com>
2021-10-27 11:20:41 -0600

    Initial commit

---
+++ sudoku.js
@@ -0,0 +1,359 @@
+boardData = document.querySelectorAll(".board textarea")
+menuOptions = document.querySelectorAll("#menu div")
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+class pointData {
+    constructor(htmlIndex) {
+        this.index = transform(htmlIndex)
+        this.x = indexToX(this.index)
+        this.y = indexToY(this.index)
+    }
+}
+
+let lookupTable = new Array(81)
+
+for (let i = 0; i < 81; i++) {
+    lookupTable[i] = new pointData(i)
+}
+
+// for (let i = 0; i < 81; i++) {
+//     console.log(lookupTable[i])
+// }
+
+let menuOptionSelected = null
+let selectedHTMLIndex = null
+board = new Array(9 * 9)
+
+for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+        board[i + j * 9] = null
+    }
+}
+boardData.forEach((index, n) => {
+    index.value = board[n] || null;
+})
+
+// function isValid(x, y, num) {
+//     if (board[x + y * 9] != null) {
+//         return false;
+//     }
+//     let i
+//     for (i = 0; i < 9; i++) {
+//         if (board[i + y * 9] === num && i !== x) {
+//             return false;
+//         }
+//     }
+//     for (i = 0; i < 9; i++) {
+//         if (board[x + i * 9] === num && i !== y) {
+//             return false;
+//         }
+//     }
+//     let htmlCoord = transform(x + y * 9)
+//     let start = htmlCoord - htmlCoord % 9
+//     for (i = start; i < start + 9; i++) {
+//         if (boardData[i].value === num.toString() && htmlCoord !== i) {
+//             return false;
+//         }
+//     }
+//     return true
+// }
+
+function isValid(htmlIndex, num) {
+    let x = lookupTable[htmlIndex].x
+    let y = lookupTable[htmlIndex].y
+    if (board[x + y * 9] != null) {
+        return false;
+    }
+    let i
+    for (i = 0; i < 9; i++) {
+        if (board[i + y * 9] === num && i !== x) {
+            return false;
+        }
+    }
+    for (i = 0; i < 9; i++) {
+        if (board[x + i * 9] === num && i !== y) {
+            return false;
+        }
+    }
+    let start = htmlIndex - htmlIndex % 9
+    for (i = start; i < start + 9; i++) {
+        if (board[lookupTable[i].index] === num && i !== htmlIndex) {
+            return false;
+        }
+    }
+    return true
+}
+
+function generateSolution(i = 0, limit = 81) {
+    if (limit === 0) {
+        return true
+    }
+    if (i > 80) {
+        i = 0
+    }
+    if (board[lookupTable[i].index] !== null) {
+        return generateSolution(i + 1, limit - 1);
+    }
+    let start = Math.floor(Math.random() * 9) + 1;
+    //let start = 1;
+    for (let n = 0; n < 9; n++) {
+        if (isValid(i, start)) {
+            board[lookupTable[i].index] = start
+            // boardData[i].value = start
+            if (!generateSolution(i + 1, limit - 1)) {
+                board[lookupTable[i].index] = null
+                // boardData[i].value = null
+            } else {
+                place(i, start)
+                return true
+            }
+        }
+        start++
+        if (start > 9) {
+            start = 1
+        }
+    }
+    return false
+}
+
+function hasMultipleSolutions(i = 0, limit = 81) {
+    if (limit === 0) {
+        return 1
+    }
+    if (board[lookupTable[i].index] !== null) {
+        return hasMultipleSolutions(i + 1, limit - 1);
+    }
+    let numOfSolutions = 0
+    for (let n = 1; n <= 9; n++) {
+        if (isValid(i, n)) {
+            board[lookupTable[i].index] = n
+            if ((numOfSolutions += hasMultipleSolutions(i + 1, limit - 1)) > 1) {
+                board[lookupTable[i].index] = null
+                return numOfSolutions
+            }
+            board[lookupTable[i].index] = null
+        }
+    }
+    return numOfSolutions
+}
+
+
+function generateProblem() {
+    generateSolution()
+    let limit = 0
+    while (limit !== 50) {
+        // await sleep(1)
+        let randIndex = Math.floor(Math.random() * 81)
+        //randIndex = limit
+        //let temp = board[lookupTable[start[limit]].index]
+        let temp = board[lookupTable[randIndex].index]
+        // if(temp==null){
+        //     limit++
+        //     continue
+        // }
+        while((temp = board[lookupTable[randIndex].index])==null){
+            randIndex = Math.floor(Math.random() * 81)
+        }
+        remove(randIndex)
+        if (hasMultipleSolutions() > 1) {
+            place(randIndex, temp)
+        }else {
+            limit++
+        }
+    }
+}
+
+// generateProblem(1)
+
+function solve() {
+    document.getElementById("solve").disabled = true;
+    document.getElementById("clearBoard").disabled = true;
+    // await fill(Math.floor(Math.random() * 81), 81)
+    generateSolution()
+    document.getElementById("solve").disabled = false;
+    document.getElementById("clearBoard").disabled = false;
+}
+
+function clearBoard() {
+    document.getElementById("solve").disabled = true;
+    document.getElementById("clearBoard").disabled = true;
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            remove(i + j * 9)
+        }
+    }
+    document.getElementById("solve").disabled = false;
+    document.getElementById("clearBoard").disabled = false;
+}
+
+// function place(x, y, num) {
+//     if (isValid(x, y, num)) {
+//         board[x + y * 9] = num
+//         boardData[transform(x + y * 9)].value = num
+//         return true
+//     } else {
+//         return false;
+//     }
+// }
+
+function place(htmlIndex, num) {
+    board[lookupTable[htmlIndex].index] = num
+    boardData[htmlIndex].value = num
+}
+
+function validPlace(htmlIndex, num) {
+    if (isValid(htmlIndex, num)) {
+        board[lookupTable[htmlIndex].index] = num
+        boardData[htmlIndex].value = num
+        return true
+    } else {
+        return false;
+    }
+}
+
+// function remove(x, y) {
+//     board[x + y * 9] = null
+//     boardData[transform(x + y * 9)].value = null
+// }
+
+function remove(htmlIndex) {
+    board[lookupTable[htmlIndex].index] = null
+    boardData[htmlIndex].value = null
+}
+
+function transform(n) {
+    let index = Math.floor(n / 9) % 3
+    let sub = Math.floor((n % 9) / 3)
+    if (index === 0) {
+        if (sub === 1) {
+            return n + 6
+        } else if (sub === 2) {
+            return n + 12
+        } else {
+            return n
+        }
+    } else if (index === 1) {
+        if (sub === 0) {
+            return n - 6
+        } else if (sub === 2) {
+            return n + 6
+        } else {
+            return n
+        }
+    } else if (index === 2) {
+        if (sub === 0) {
+            return n - 12
+        } else if (sub === 1) {
+            return n - 6
+        } else {
+            return n
+        }
+    }
+}
+
+function codeCoordsToHtmlIndex(x, y) {
+    return transform(x + y * 9)
+}
+
+function indexToX(n) {
+    return n % 9
+}
+
+function indexToY(n) {
+    return Math.floor(n / 9)
+}
+
+function toIndex(x, y) {
+    return x + y * 9
+}
+
+function getNumberFromKeyEvent(event) {
+    if (event.keyCode >= 96 && event.keyCode <= 105) {
+        return event.keyCode - 96;
+    } else if (event.keyCode >= 48 && event.keyCode <= 57) {
+        return event.keyCode - 48;
+    }
+    return null;
+}
+
+function toggleCross(htmlIndex, center = true) {
+    if (center) {
+        boardData[htmlIndex].classList.toggle('selected')
+    }
+    for (let i = 0; i < 9; i++) {
+        if (lookupTable[htmlIndex].x !== i) {
+            boardData[codeCoordsToHtmlIndex(i, lookupTable[htmlIndex].y)].classList.toggle('sub-selected')
+        }
+    }
+    for (let j = 0; j < 9; j++) {
+        if (lookupTable[htmlIndex].y !== j) {
+            boardData[codeCoordsToHtmlIndex(lookupTable[htmlIndex].x, j)].classList.toggle('sub-selected')
+        }
+    }
+}
+
+boardData.forEach((index, htmlIndex) => {
+    index.addEventListener("keydown", (e) => {
+        e.preventDefault();
+        e.stopPropagation()
+        if (e.keyCode === 8 || e.keyCode === 46) {
+            remove(htmlIndex)
+            return
+        }
+        let num;
+        if ((num = getNumberFromKeyEvent(e)) !== null) {
+        if(isValid(htmlIndex, num)) {
+            if (index.value !== null) {
+                remove(htmlIndex)
+                place(htmlIndex, num)
+            } else {
+                place(htmlIndex, num)
+            }
+        }
+            // console.log("t")
+        } else {
+            // console.log("not num")
+        }
+    })
+    index.addEventListener("click", (e) => {
+        e.stopPropagation()
+        if (selectedHTMLIndex != null) {
+            toggleCross(selectedHTMLIndex)
+        }
+        toggleCross(htmlIndex, false)
+        index.classList.toggle("selected")
+        selectedHTMLIndex = htmlIndex
+        if (menuOptionSelected != null) {
+            if (isValid(htmlIndex, menuOptions[menuOptionSelected - 1].innerHTML)) {
+                place(htmlIndex, menuOptions[menuOptionSelected - 1].innerHTML)
+            } else {
+                remove(htmlIndex)
+            }
+        }
+    })
+})
+
+window.addEventListener("click", () => {
+    if (selectedHTMLIndex != null) {
+        toggleCross(selectedHTMLIndex)
+        selectedHTMLIndex = null
+    }
+    if (menuOptionSelected != null) {
+        menuOptions[menuOptionSelected - 1].classList.toggle("selected")
+        menuOptionSelected = null
+    }
+})
+
+menuOptions.forEach((index, n) => {
+    index.addEventListener("click", (e) => {
+        e.stopPropagation()
+        if (menuOptionSelected != null) {
+            menuOptions[menuOptionSelected - 1].classList.toggle("selected")
+        }
+        menuOptionSelected = n + 1
+        index.classList.toggle("selected")
+    })
+})
